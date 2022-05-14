5f47cd3d03b616b47d53175f3f65824cd2fde5d8
roshnirifa <roshnirifa998@gmail.com>
2022-05-14 12:26:22 +0600

    banner added

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ function App() {
       <Header></Header>
       <Routes>
         <Route path='/' element={<Home></Home>}></Route>
+        <Route path='/home' element={<Home></Home>}></Route>
         <Route path='about' element={<About></About>}></Route>
         <Route path='login' element={<Login></Login>}></Route>
       </Routes>
--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom'
 const Header = () => {
     const menuItems =
         <>
-            <li><Link to='/'>Home</Link></li>
+            <li><Link to='/home'>Home</Link></li>
             <li><Link to='/about'>About</Link></li>
             <li><Link to='/appoitment'>Appoitment</Link></li>
             <li><Link to='/reviews'>Reviews</Link></li>
