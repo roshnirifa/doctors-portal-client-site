f86887cc0ba4988b1fdba529c02c9807c35701b1
roshnirifa <roshnirifa998@gmail.com>
2022-05-22 20:48:53 +0600

    appoinment reload problem sloved

--- src/Pages/Appoinment/AvailableAppoinments.js
+++ src/Pages/Appoinment/AvailableAppoinments.js
@@ -1,27 +1,36 @@
 import { format } from 'date-fns';
 import React, { useState, useEffect } from 'react';
+import { useQuery } from 'react-query';
 import Service from '../Service/Service';
+import Loading from '../Shared/Loading/Loading';
 
 import BookingModal from './BookingModal';
 
 
 const AvailableAppoinments = ({ date }) => {
-    const [services, setServices] = useState([]);
+    // const [services, setServices] = useState([]);
     const [treatment, setTreatment] = useState(null);
-
-
-    useEffect(() => {
-        fetch('http://localhost:5000/service')
+    const formatedDate = format(date, 'PP');
+    const { data: services, isLoading, refetch } = useQuery(['available', formatedDate], () =>
+        fetch(`http://localhost:5000/available?date=${formatedDate}`)
             .then(res => res.json())
-            .then(data => setServices(data));
+    )
+    if (isLoading) {
+        return <Loading></Loading>
     }
-        , [])
+
+
+    // useEffect(() => {
+    //     fetch(`http://localhost:5000/available?date=${formatedDate}`)
+    //         .then(res => res.json())
+    //         .then(data => setServices(data));
+    // }, [formatedDate])
     return (
         <div>
             <p className='text-secondary font-bold text-center text-xl'>Available Appoinment on{format(date, 'PP')}</p>
             <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5  px-12'>
                 {
-                    services.map(service => <Service
+                    services?.map(service => <Service
                         key={service._id}
                         service={service}
                         setTreatment={setTreatment}
@@ -32,7 +41,8 @@ const AvailableAppoinments = ({ date }) => {
                     treatment && <BookingModal
                         date={date}
                         treatment={treatment}
-                        setTreatment={setTreatment}></BookingModal>
+                        setTreatment={setTreatment}
+                        refetch={refetch}></BookingModal>
                 }
             </div>
 
--- src/Pages/Service/Service.js
+++ src/Pages/Service/Service.js
@@ -1,26 +1,26 @@
 import React from 'react';
 
 const Service = ({ service, setTreatment }) => {
-    const { name, slots } = service
+    const { name, available } = service
     return (
         <div className="card w-96 bg-base-100 shadow-xl">
             <div className="card-body text-center">
                 <h2 className="card-title justify-center text-secondary text-center">{name}</h2>
                 <p>
                     {
-                        slots.length > 0 ?
+                        available.length > 0 ?
                             <span>
                                 {
-                                    slots[0]
+                                    available[0]
                                 }
                             </span>
                             :
                             <span className='text-red-500'>No Slots Available</span>
                     }
                 </p>
-                <p> {slots.length} {slots.length > 1 ? 'Space Available' : 'Space Available'}</p>
+                <p> {available.length} {available.length > 1 ? 'Space Available' : 'Space Available'}</p>
                 <div class="card-actions justify-center">
-                    <label for="booking-modal" disabled={slots.length === 0}
+                    <label for="booking-modal" disabled={available.length === 0}
                         onClick={() => setTreatment(service)} class=" modal-button btn btn-secondary text-white uppercase">Book Appointment</label>
                 </div>
             </div>
